48c2b786b4faf7d07f06e4884fff7af681effdf0
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2025-11-07 14:14:10 +0200

    feat: setup terraform w/ github actions

---
+++ infra/env/prod/main.tf
@@ -0,0 +1,15 @@
+resource "aws_budgets_budget" "spend_budget" {
+  name         = "SpendBudget"
+  budget_type  = "COST"
+  limit_amount = "0.1"
+  limit_unit   = "USD"
+  time_unit    = "MONTHLY"
+
+  notification {
+    comparison_operator        = "GREATER_THAN"
+    threshold                  = 0
+    threshold_type             = "ABSOLUTE_VALUE"
+    notification_type          = "ACTUAL"
+    subscriber_email_addresses = ["test@test.com"]
+  }
+}
---
+++ infra/env/prod/providers.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  region = "eu-north-1"
+}
+
+terraform {
+  required_version = "1.13.5"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.2.0"
+    }
+  }
+
+  backend "s3" {
+    bucket       = "tfstate-accountant-bot"
+    key          = "prod/terraform.tfstate"
+    region       = "eu-north-1"
+    use_lockfile = true
+    encrypt      = true
+  }
+}
