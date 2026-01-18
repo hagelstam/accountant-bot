fee32bdee40f83797baf7ef9570f9bc57deccf25
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2026-01-19 00:33:14 +0200

    refactor: split up terraform

--- infra/env/prod/main.tf
+++ infra/env/prod/main.tf
@@ -1,27 +1,3 @@
-variable "telegram_bot_token" {
-  type        = string
-  description = "Telegram bot token"
-  sensitive   = true
-}
-
-variable "google_credentials_json" {
-  type        = string
-  description = "Google credentials JSON"
-  sensitive   = true
-}
-
-variable "google_spreadsheet_id" {
-  type        = string
-  description = "Google spreadsheet ID"
-  sensitive   = true
-}
-
-variable "image_tag" {
-  type        = string
-  description = "Docker image tag to deploy"
-  default     = "latest"
-}
-
 module "bot" {
   source = "../../modules/bot"
 
@@ -30,7 +6,7 @@ module "bot" {
   telegram_bot_token      = var.telegram_bot_token
   google_credentials_json = var.google_credentials_json
   google_spreadsheet_id   = var.google_spreadsheet_id
-  log_retention_days      = 7
+  log_retention_days      = 3
 
   tags = {
     Project     = "accountant-bot"
