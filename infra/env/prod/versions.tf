18305fb41bd5518e83bd6fefe73d3247479bb8b6
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2026-03-22 09:25:50 +0200

    chore(deps): bump terraform deps

--- infra/env/prod/versions.tf
+++ infra/env/prod/versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = "1.14.5"
+  required_version = "1.14.7"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 6.32.1"
+      version = "~> 6.37.0"
     }
   }
 
